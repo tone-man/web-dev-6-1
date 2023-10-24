9ba80e32136554637d7db6be618ef9c67374fdcc
tones <thetoneman@outlook.com>
2023-10-24 10:44:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a trivial example using node.js and the get request to spin up html, using the http and url libraries.
---
+++ index.js
@@ -0,0 +1,39 @@
+let http = require('http');
+let url = require('url');
+
+// Basic use case
+http.createServer( function (req, res) {
+    let query = url.parse(req.url, true); // Use url library to parse url
+    
+    if (query.pathname =='/') {
+        res.writeHead(200 , {'Content-Type': "text/html"});
+        res.write("<h1> Hello World! </h1>"); // This is showing that it is html.
+        res.write(JSON.stringify(url));
+        res.end();
+    }
+    else {
+        res.writeHead(200 , {'Content-Type': "text/html"});
+        res.write("<h1> Your query is! </h1>"); // This is showing that it is html.
+        res.write(JSON.stringify(query));
+        res.end();
+    }
+}).listen(5000);
+
+/** 
+ * This server will cause an err due to an attempt to
+ * write to stream after res.end()
+*/
+/*
+http.createServer(function (req, res) {
+
+    if (req.url) {
+        res.writeHead(200 , {'Content-Type': "text/html"});
+        res.write("<h1> Hello World! </h1>");
+        res.end();
+    }
+
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.write("No page found.")
+
+}).listen(5000);
+*/
